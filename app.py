a38f0283d28561b94b56f90dfba6eb1cab8e92c1
Andrew D'Amico <adamico@ieee.org>
2022-10-29 17:43:59 -0500

    created hello world QT app

---
+++ app.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QLabel
+from PyQt5.uic import loadUi
+from main_ui import Ui_MainWindow
+
+
+class Window(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, parent=None):
+
+        super().__init__(parent)
+
+        self.setupUi(self)
+
+
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    
+    x = "Hello, World!"
+    win.label.setText(x)
+
+    sys.exit(app.exec())
---
+++ main_ui.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/main.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 631, 91))
+        self.label.setLineWidth(1)
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 933, 29))
+        self.menubar.setObjectName("menubar")
+        self.menuObservation_Writer_and_Labeler = QtWidgets.QMenu(self.menubar)
+        self.menuObservation_Writer_and_Labeler.setObjectName("menuObservation_Writer_and_Labeler")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuObservation_Writer_and_Labeler.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.menuObservation_Writer_and_Labeler.setTitle(_translate("MainWindow", "Observation Writer and Labeler"))
